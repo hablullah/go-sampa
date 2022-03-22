b7f7b5dd689dc69d88c33f37105f2c4c2900e670
Radhi Fadlillah <m.radhi.f@gmail.com>
2022-03-22 16:59:19 +0700

    Better sample code

--- root.go
+++ root.go
@@ -6,7 +6,6 @@ import (
 )
 
 func main() {
-	// Get initial sun data
 	location := Location{
 		Latitude:    34.1667,
 		Longitude:   -118.4667,
@@ -16,24 +15,24 @@ func main() {
 
 	opts := &SunOptions{
 		Pressure:               1010,
-		SurfaceSlope:           30,
-		SurfaceAzimuthRotation: -10,
-		DeltaT:                 67,
+		SurfaceSlope:           0,
+		SurfaceAzimuthRotation: -0,
+		DeltaT:                 69.8,
 	}
 
 	tz := time.FixedZone("LST", -8*60*60)
 	dt := time.Date(2010, 1, 1, 0, 0, 0, 0, tz)
-	// data, _ := GetSunAtTime(dt, location, opts)
+	limit := time.Date(2023, 1, 1, 0, 0, 0, 0, tz)
 
-	// bt, _ := json.MarshalIndent(&data, "", "\t")
-	// fmt.Println(string(bt))
-
-	transit, _ := GetSunTransit(dt, location, opts)
-	fmt.Printf("TRANSIT: %s, elevation: %f\n", transit.DateTime, transit.TopocentricElevationAngle)
-
-	sunrise, _ := GetSunAtElevation(-0.8333, transit, true, location, opts)
-	fmt.Printf("SUNRISE: %s, elevation: %f\n", sunrise.DateTime, sunrise.TopocentricElevationAngle)
-
-	sunset, _ := GetSunAtElevation(-0.8333, transit, false, location, opts)
-	fmt.Printf("SUNSET: %s, elevation: %f\n", sunset.DateTime, sunset.TopocentricElevationAngle)
+	for dt.Before(limit) {
+		sunrise, _ := GetSunAtElevation(dt, -0.8333, true, location, opts)
+		transit, _ := GetSunTransit(dt, location, opts)
+		sunset, _ := GetSunAtElevation(dt, -0.8333, false, location, opts)
+		fmt.Printf("%s\t%s\t%s\t%s\n",
+			dt.Format("2006-01-02"),
+			sunrise.DateTime.Format("15:04:05"),
+			transit.DateTime.Format("15:04:05"),
+			sunset.DateTime.Format("15:04:05"))
+		dt = dt.AddDate(0, 0, 1)
+	}
 }
